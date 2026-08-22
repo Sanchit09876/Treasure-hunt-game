6d4391ef5ca9efcff9ecbc6ad64da4c8dbbc6f8f
Sanchit Maharjan <sanchitmaharjan20@gmail.com>
2026-08-22 14:07:09 +0545

    Add game config settings in difficutlyConfig.js

---
+++ src/data/difficutlyConfig.js
@@ -0,0 +1,18 @@
+const difficutlyConfig = {
+  easy: {
+    skull: [1, 2],
+    multipliers: [1.05, 1.1, 1.15, 1.25],
+  },
+  medium: {
+    skull: [2, 3],
+    multipliers: [1.15, 1.2, 1.25, 1.35],
+  },
+  hard: {
+    skull: [3 - 4],
+    multiplier: [1.25, 1.3, 1.35, 1, 45],
+  },
+  extreme: {
+    skull: [5, 5],
+    multipler: [1.5, 1.75, 2, 2, 25],
+  },
+};
